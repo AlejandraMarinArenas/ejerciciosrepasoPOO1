636bad7c600d62f06b3394b38120e15cd286b899
AlejandraMarinArenas <110303894+AlejandraMarinArenas@users.noreply.github.com>
2023-02-10 10:00:57 -0500

    h

--- ejercicios/Ejercicio3.py
+++ ejercicios/Ejercicio3.py
@@ -1,2 +1,12 @@
 if __name__=="__main__":
     import random
+
+    list = []
+
+    random_numbers = int(input("enter the number of numbers you want to see randomly: "))
+
+    for i in range (random_numbers):
+        list.append(random.randrange(10000))
+
+
+    print(list)
